15b92f4d0d6638a6244a36fefec13e9522b2081c
jun <1010479088@qq.com>
2020-12-22 10:42:17 +0800

    1.小改动

--- src/Tester.java
+++ src/Tester.java
@@ -1,8 +1,19 @@
-import ui.RenamePage;
+import presenter.GoProRenamer;
+import utils.BasePage;
 
 public class Tester {
 
     public static void main(String[] args) {
-        new RenamePage().showUI();
+        new BasePage() {
+            @Override
+            protected void action(String path) {
+                new GoProRenamer(path, this::log);
+            }
+
+            @Override
+            protected String title() {
+                return "GoPro重命名!";
+            }
+        }.showUI();
     }
 }
--- src/utils/BasePage.java
+++ src/utils/BasePage.java
@@ -1,27 +1,34 @@
-package ui;
-
-import presenter.GoProRenamer;
-import utils.TextUtil;
+package utils;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class RenamePage extends JFrame {
+public abstract class BasePage extends JFrame {
     private JTextArea mLoggerView;
     private StringBuilder builder = new StringBuilder();
+
+    protected abstract void action(String path);
+
+    protected abstract String title();
 
     public void showUI() {
         init();
         configUI();
     }
 
+    protected void log(String content) {
+        builder.append(content);
+        builder.append("\n");
+        mLoggerView.setText(builder.toString());
+    }
+
     private void init() {
         setLocation(200, 100);
         setSize(500, 300);
         setVisible(true);
-        setTitle("GoPro重命名!");
+        setTitle(title());
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -40,7 +47,6 @@ public class RenamePage extends JFrame {
             rename(path);
         });
         mLoggerView = new JTextArea();
-        mLoggerView.setText("大俊子在此!");
         new JScrollPane(mLoggerView);
         add(BorderLayout.CENTER, mLoggerView);
     }
@@ -49,13 +55,8 @@ public class RenamePage extends JFrame {
         if (TextUtil.isEmpty(path)) {
             log("文件夹为空");
         } else {
-            new GoProRenamer(path, this::log);
+            builder.setLength(0);
+            action(path);
         }
-    }
-
-    private void log(String content) {
-        builder.append(content);
-        builder.append("\n");
-        mLoggerView.setText(builder.toString());
     }
 }
